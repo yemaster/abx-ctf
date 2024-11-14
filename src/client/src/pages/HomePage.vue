d0d26b7e5d7e79b9c0c18fa52ffe448d19321de4
yemaster <me@yemaster.cn>
2024-11-15 00:06:37 +0800

    Add homepage contest list style

--- src/client/src/pages/HomePage.vue
+++ src/client/src/pages/HomePage.vue
@@ -24,16 +24,111 @@ onMounted(async () => {
             <div class="ax-header-2">
                 近期比赛
             </div>
-            <div class="ax-card">
+            <div class="ax-contest-box">
+                <div class="ax-contest-box__image">
+                    <img
+                        src="https://ctf.u5tc.cn/assets/ef3ee80c56ae6b1e36a73dda2292bf3780bab8d446ffddbd37a35994e1795caa/poster">
+                </div>
                 <div class="ax-card-body">
                     <div class="ax-card-body__header">
-                        欢迎来到 Abstrax CTF
+                        测试比赛
                     </div>
-                    <div class="ax-card-body__content">
-                        欢迎来到 Abstrax CTF
+                    <div class="ax-card-body__meta">
+                        <n-flex size="small">
+                            <n-tag :bordered="false" size="small" :color="{ color: '#a333c815', textColor: '#a333c8' }">
+                                团队赛
+                            </n-tag>
+                            <n-popover trigger="hover">
+                                <template #trigger>
+                                    <n-tag :bordered="false" type="warning" size="small">
+                                        开始于3小时后
+                                    </n-tag>
+                                </template>
+                                <span>2024/11/14 23:56</span>
+                            </n-popover>
+                        </n-flex>
+                    </div>
+                </div>
+            </div>
+            <div class="ax-contest-box">
+                <div class="ax-contest-box__image">
+                    <img
+                        src="https://ctf.u5tc.cn/assets/ef3ee80c56ae6b1e36a73dda2292bf3780bab8d446ffddbd37a35994e1795caa/poster">
+                </div>
+                <div class="ax-card-body">
+                    <div class="ax-card-body__header">
+                        测试比赛
+                    </div>
+                    <div class="ax-card-body__meta">
+                        <n-flex size="small">
+                            <n-tag :bordered="false" size="small" :color="{ color: '#00b5ad15', textColor: '#00b5ad' }">
+                                个人赛
+                            </n-tag>
+                            <n-popover trigger="hover">
+                                <template #trigger>
+                                    <n-tag :bordered="false" type="error" size="small">
+                                        3小时后结束
+                                    </n-tag>
+                                </template>
+                                <span>2024/11/14 23:56</span>
+                            </n-popover>
+                        </n-flex>
+                    </div>
+                </div>
+            </div>
+            <div class="ax-contest-box">
+                <div class="ax-contest-box__image">
+                    <img
+                        src="https://ctf.u5tc.cn/assets/ef3ee80c56ae6b1e36a73dda2292bf3780bab8d446ffddbd37a35994e1795caa/poster">
+                </div>
+                <div class="ax-card-body">
+                    <div class="ax-card-body__header">
+                        测试比赛
+                    </div>
+                    <div class="ax-card-body__meta">
+                        <n-flex size="small">
+                            <n-tag :bordered="false" size="small" :color="{ color: '#00b5ad15', textColor: '#00b5ad' }">
+                                个人赛
+                            </n-tag>
+                            <n-popover trigger="hover">
+                                <template #trigger>
+                                    <n-tag :bordered="false" type="info" size="small">
+                                        结束于3小时前
+                                    </n-tag>
+                                </template>
+                                <span>2024/11/14 23:56</span>
+                            </n-popover>
+                        </n-flex>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+.ax-contest-box {
+    width: 100%;
+    background-color: #fff;
+    border-radius: 8px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.15);
+}
+
+.ax-contest-box .ax-card-body {
+    padding: 1rem 1.5rem;
+}
+
+.ax-contest-box__image {
+    width: 100%;
+    height: 8rem;
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+    overflow: hidden;
+}
+
+.ax-contest-box__image img {
+    width: 100%;
+    height: 8rem;
+    overflow: hidden;
+}
+</style>
